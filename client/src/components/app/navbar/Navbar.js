a07ddf3faa8c799b2a3bd1063786fd869591c8d3
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2019-12-25 01:05:27 +0530

    Added bulma css to the navbar

--- client/src/components/app/navbar/Navbar.js
+++ client/src/components/app/navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import './navbar.css'
+import 'bulma/css/bulma.css'
 import logo from '../../img/logo.jpeg'
 import { Link } from 'react-router-dom'
 import url from '../../config'
 
+
 function Navbar () {
   const [account, setAccount] = useState('')
   const sid = JSON.parse(window.localStorage.getItem('session'))
-  
+
   useEffect(() => {
     async function getDataFromDb () {
       let data = await window.fetch(`${url}/account/?sid=${sid}`)
@@ -19,20 +20,31 @@ function Navbar () {
 
   return (
     <>
-      <nav className='navbar'>
-        <Link className='navbar-brand' to='/loggedIn'>
-          <label className='logo-text'>SpaceRep</label>
-          <img src={logo} alt='logo' className='logo' />
-        </Link>
 
-        <div className='navbar-menu-start'>
-          <Link className='navbar-item' to='/decks'> Decks</Link>
-          <Link className='navbar-item' to='/add'> Add</Link>
+      <nav className='navbar' role='navigation' aria-label='main navigation'>
+        <div className='navbar-brand'>
+          <Link className='navbar-item' to='/loggedIn'>
+            <img src={logo} alt='logo' />
+            <label className='logo-text'>SpaceRep</label>
+          </Link>
+
+          <Link role='button' className='navbar-burger' aria-label='menu' aria-expanded='false'>
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+          </Link>
         </div>
 
-        <div className='navbar-menu-end'>
-          <Link className='navbar-item' to='/decks'>Account- {account}</Link>
-          <Link className='navbar-item' to='/login'> Log Out</Link>
+        <div className='navbar-menu'>
+          <div className='navbar-start'>
+            <Link className='navbar-item' to='/decks'> Decks</Link>
+            <Link className='navbar-item' to='/add'> Add</Link>
+          </div>
+
+          <div className='navbar-end'>
+            <Link className='navbar-item' to='/decks'>Account- {account}</Link>
+            <Link className='navbar-item' to='/login'>  Log Out</Link>
+          </div>
         </div>
       </nav>
     </>
