41b7d19b3350508d33ec3070bd64ef1e0b638de1
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2019-12-29 20:44:57 +0530

    nandini not doing anything

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/dist'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 
