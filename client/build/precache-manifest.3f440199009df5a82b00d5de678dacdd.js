825226c061a7b6474a24bdda59c8c25104099099
nandinivenkateshan <nandinivenkatesan809@gmail.com>
2019-12-30 12:16:23 +0530

    Removed package-lock.json

---
+++ client/build/precache-manifest.3f440199009df5a82b00d5de678dacdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "048aab9b7d768dc7a3179b07be26feaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc065e2d03260e983e6e",
+    "url": "/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "e0b2d1145ee5f329ef1d",
+    "url": "/static/css/main.4e06cedd.chunk.css"
+  },
+  {
+    "revision": "cc065e2d03260e983e6e",
+    "url": "/static/js/2.9fcedb8f.chunk.js"
+  },
+  {
+    "revision": "e0b2d1145ee5f329ef1d",
+    "url": "/static/js/main.98aea82b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b1ba95503271afe81be024431085e48a",
+    "url": "/static/media/logo.b1ba9550.jpeg"
+  }
+]);
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -15,17 +15,17 @@ function Login () {
   }
 
   const [values, setValues] = useState({})
-  const [errors, setErrors] = useState({})
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [isLogin, setIsLogin] = useState(false)
   const [errMsg, setErrMsg] = useState({})
   const [netErr, setNetErr] = useState(false)
 
   useEffect(() => {
-    if (Object.keys(errors).length === 0 && isSubmitting) {
+    if (isSubmitting) {
+      console.log('val', values)
       fetchReq(`${url}/login`, values)
     }
-  }, [errors])
+  }, [isSubmitting])
 
   const fetchReq = async (url, card) => {
     const response = await fetchPost(url, card)
@@ -44,25 +44,13 @@ function Login () {
 
   const handleChange = e => {
     e.persist()
-    setValues(values => ({ ...values, [e.target.name]: e.target.value }))
+    setValues(values =>
+      ({ ...values, [e.target.name]: e.target.value }))
   }
 
   const handleSubmit = e => {
     e.preventDefault()
     setIsSubmitting(true)
-    setErrors(validate(values))
-  }
-
-  const validate = values => {
-    const errors = {}
-    if (!/\S+@\S+\.\S+/.test(values.user_email)) {
-      errors.user_email = 'Email address is invalid'
-    }
-    if (!values.user_email) {
-      errors.user_email = 'User Email is required'
-    }
-    if (!values.pswd) errors.pswd = 'Password is required'
-    return errors
   }
 
   return (
@@ -76,31 +64,25 @@ function Login () {
             <h1 className='heading'>Login</h1>
             <p className='sub-heading'>Log in to an existing account.</p>
             {errMsg &&
-              <p className='acc-not-exist-msg'> {errMsg.res}
+              <p className='err-msg'> {errMsg.res}
               </p>}
             <form className='login-form' onSubmit={e => handleSubmit(e)}>
               <label htmlFor='email' className='label'>Email</label>
               <input
                 type='email' placeholder='Enter your Email'
-                id='email' name='user_email' className={`input${errors.user_email && 'invalid'}`}
+                id='email' name='user_email' className='input'
                 onChange={(e) => handleChange(e)}
                 value={values.user_email || ''}
                 required
               />
-              {errors.user_email && (
-                <p className='invalid-para'>{errors.user_email}</p>
-              )}
               <label htmlFor='pswd' className='label'>Password</label>
               <input
                 type='password' id='pswd' name='pswd'
-                className={`input${errors.pswd && 'invalid'}`} placeholder='Enter the password'
+                className='input' placeholder='Enter the password'
                 onChange={(e) => handleChange(e)}
                 value={values.pswd || ''}
                 required
               />
-              {errors.pswd && (
-                <p className='invalid-para'>{errors.pswd}</p>
-              )}
               <button className='login-btn'>Login</button>
               {isLogin &&
                 <Redirect to='/decks' />}
